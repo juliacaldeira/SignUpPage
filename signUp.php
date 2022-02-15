03b0d4c65420d14d76daaae318d52b7f6299b6c4
JuliaCaldeira <julia.pcfranca@gmail.com>
2022-02-14 22:41:14 -0300

    commit inicial

---
+++ signUp.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="signUp.css" type="text/css">
+    <link rel="icon" type="image/ico" href="splash.ico">
+    <title>Sign In</title>
+</head>
+<body>
+<div id="box">
+        <form>
+            <h2>Login</h2>
+            <div id="logos">
+            <img src="logoG.png" class="logo"><img src="logoF.png" class="logo">
+            </div> 
+            <label for="name"> Name
+                <input id="name" name="name" type="text" required>
+            </label>
+            <label for="password"> Password
+                <input id="password" name="password" type="password" required>
+            </label>
+            <button id="but">Sign Up</button>
+        </form>
+    <div id="options">
+        <img src="">
+    </div>
+</div>
+</body>
+</html>
